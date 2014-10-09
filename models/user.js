69b4902cfadc275138039382162680ee2a1893d9
littlechun4 <littelchun4@naver.com>
2014-10-09 22:43:00 +0900

    login count added to user model

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
     user_id: String,
     password: String,
+    login_count: Number
 });
 
 module.exports = mongoose.model('User', userSchema);
